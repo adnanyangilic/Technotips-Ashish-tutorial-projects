c83c5303a44acc8e1779f9de711de3c09b43e6b9
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-12 17:15:45 +0100

    Remove unnecessary Classes, Comments and NuGet Packages, Packages Folder and BIN Folder from TextBoxnDropdownnHyperlinksWithHTMLHelper

--- 10_TextBoxnDropdownnHyperlinksWithHTMLHelper/TextBoxnDropdownnHyperlinksWithHTMLHelper/Models/ViewModels/EmployeeViewModel.cs
+++ 10_TextBoxnDropdownnHyperlinksWithHTMLHelper/TextBoxnDropdownnHyperlinksWithHTMLHelper/Models/ViewModels/EmployeeViewModel.cs
@@ -2,11 +2,9 @@
 {
     public class EmployeeViewModel
     {
-        public int EmployeeID { get; set; }
         public string Name { get; set; }
         public string Adress { get; set; }
         public int DepartmentID { get; set; }
-        public string DepartmentName { get; set; }
         public bool Remember { get; set; }
     }
 }
