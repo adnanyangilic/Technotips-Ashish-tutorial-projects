7e04057c8a9c0b09fdfba1b4af5984242d4ca4e3
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-12 14:53:57 +0100

    Remove unnecessary Classes, Comments and NuGet Packages, Packages Folder and BIN Folder from HyperlinksAndButtonsExample

--- 08_HyperlinksAndButtonsExample/HyperlinksAndButtonsExample/Controllers/ExampleController.cs
+++ 08_HyperlinksAndButtonsExample/HyperlinksAndButtonsExample/Controllers/ExampleController.cs
@@ -1,20 +1,18 @@
 ﻿using HyperlinksAndButtonsExample.Models;
 using HyperlinksAndButtonsExample.Models.ViewModels;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using System.Collections.Generic;
 
 namespace HyperlinksAndButtonsExample.Controllers
 {
     public class ExampleController : Controller
     {
-        // GET: Example
         public ActionResult Index()
         {
             ViewBag.MessageHL = "Egyszerű Hyperlink example bemutatása";
             ViewBag.MessageHLB = "Hyperlink example bemutatása Button objektumként";
 
-            /// Kapcsolódás az adatbázishoz
             EmployeesDBEntities db = new EmployeesDBEntities();
 
             return View(GetEmployees(db));
@@ -22,7 +20,6 @@ namespace HyperlinksAndButtonsExample.Controllers
 
         public ActionResult EmployeeDetail(int EmployeeID)
         {
-            /// Kapcsolódás az adatbázishoz
             EmployeesDBEntities db = new EmployeesDBEntities();
 
             return View(GetSelectedEmployeeDetail(db, EmployeeID));
@@ -36,21 +33,17 @@ namespace HyperlinksAndButtonsExample.Controllers
         /// <returns>A kiválasztott Employee adatai (Név, cím, osztálynév)</returns>
         private EmployeeViewModel GetSelectedEmployeeDetail(EmployeesDBEntities db, int EmployeeID)
         {
-            /// Lekérdezzük azt az Employee objektumot az Employee táblából, amelynek az
-            /// ID-ja megegyezik a paraméterben kapott ID-val
             Employee selectedEmployee = db.Employees.SingleOrDefault(x => x.EmployeeID == EmployeeID);
 
             return GetSelectedEmployeeDetailInViewableFormat(selectedEmployee);
         }
 
         /// <summary>
-        ///     A kiválasztott Employee adatainak átalakítása a View-on megjeleníthető formában
-        ///     (Csak a szükséges adatok megtartása)
+        ///     A kiválasztott Employee adatainak átalakítása a View-on megjeleníthető formában.
         /// </summary>
         /// <param name="selectedEmployee">A lekérdezett és visszakapott Employee összes adata</param>
         /// <returns>
-        ///     A View-on megjeleníthető Employee objektum, amely a szükséges (Név, cím, osztály)
-        ///     attribútumokat tárolja
+        ///     A View-on megjeleníthető Employee objektum
         /// </returns>
         private EmployeeViewModel GetSelectedEmployeeDetailInViewableFormat(Employee selectedEmployee)
         {
@@ -70,11 +63,8 @@ namespace HyperlinksAndButtonsExample.Controllers
         /// <returns>Dolgozói adatok a View-on megjeleníthető formátumban</returns>
         private List<EmployeeViewModel> GetEmployees(EmployeesDBEntities db)
         {
-            /// Adatok lekérdezése az Employee táblából
             List<Employee> employees = db.Employees.ToList();
 
-            /// A lekérdezett adatokat átalakítjuk a View-on megjeleníthető formátumú objektummá
-            /// amely a Dolgozó Nevét és ID-ját fogja tartalmazni
             List<EmployeeViewModel> employeesInViewableFormat = employees.Select(x => new EmployeeViewModel
             {
                 EmployeeID = x.EmployeeID,
