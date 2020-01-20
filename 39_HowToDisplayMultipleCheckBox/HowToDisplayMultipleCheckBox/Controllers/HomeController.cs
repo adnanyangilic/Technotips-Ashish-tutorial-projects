79893d544109d737dafe07a48e43ddfed32d3d78
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-20 22:23:01 +0100

    Remove unnecessary Classes, Comments and NuGet Packages, Packages Folder and BIN Folder from HowToDisplayMultipleCheckBox

--- 39_HowToDisplayMultipleCheckBox/HowToDisplayMultipleCheckBox/Controllers/HomeController.cs
+++ 39_HowToDisplayMultipleCheckBox/HowToDisplayMultipleCheckBox/Controllers/HomeController.cs
@@ -8,20 +8,9 @@ namespace HowToDisplayMultipleCheckBox.Controllers
     {
         public ActionResult Index()
         {
-            /// Eltároljuk ViewBag-ben az előre definiált ShopItem listát
             ViewBag.ShopItemList = GetShopItemList();
-   
-            /// Vizsgálat, hogy volt-e már bejelentkezett felhasználó,
-            /// mert akkor a bejelentkeztetett Index-et jelenítjük meg
-            /// a User-nak
-            if (Session["UserID"] != null)
-            {
-                return RedirectToAction("../Example/Index");
-            }
-            else
-            {
-                return View();
-            }
+
+            return View();
         }
 
         /// <summary>
