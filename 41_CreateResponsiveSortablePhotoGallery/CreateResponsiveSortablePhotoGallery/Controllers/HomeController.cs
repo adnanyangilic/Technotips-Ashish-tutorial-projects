67402268004eaa00015618046c486aa7e9060aed
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-21 10:07:32 +0100

    Remove unnecessary Classes, Comments and NuGet Packages, Packages Folder and BIN Folder from CreateResponsiveSortablePhotoGallery

--- 41_CreateResponsiveSortablePhotoGallery/CreateResponsiveSortablePhotoGallery/Controllers/HomeController.cs
+++ 41_CreateResponsiveSortablePhotoGallery/CreateResponsiveSortablePhotoGallery/Controllers/HomeController.cs
@@ -6,17 +6,7 @@ namespace CreateResponsiveSortablePhotoGallery.Controllers
     {
         public ActionResult Index()
         {
-            /// Vizsgálat, hogy volt-e már bejelentkezett felhasználó,
-            /// mert akkor a bejelentkeztetett Index-et jelenítjük meg
-            /// a User-nak
-            if (Session["UserID"] != null)
-            {
-                return RedirectToAction("../Example/Index");
-            }
-            else
-            {
-                return View();
-            }
+            return View();
         }
     }
 }
