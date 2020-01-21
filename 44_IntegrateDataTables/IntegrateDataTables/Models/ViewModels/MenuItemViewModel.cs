aca3883c018395f3ea656c44acb783184e4ccbdf
Sullivan008 <kozakpeter0424@gmail.com>
2020-01-21 20:06:30 +0100

    Remove unnecessary Classes, Comments and NuGet Packages, Packages Folder and BIN Folder from IntegrateDataTables

--- 44_IntegrateDataTables/IntegrateDataTables/Models/ViewModels/MenuItemViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IntegrateDataTables.Models.ViewModels
-{
-    public class MenuItemViewModel
-    {
-        public string LinkName { get; set; }
-
-        public string Link { get; set; }
-    }
-}
